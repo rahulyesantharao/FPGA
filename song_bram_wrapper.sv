484828c98e45257ff8f4487443c3a1cf24785e62
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2019-12-13 06:31:46 -0500

    remove dead file

--- song_bram_wrapper.sv
+++
@@ -1,8 +0,0 @@
-// TODO: convert into a RAM, instead of ROM
-module song_bram_wrapper(
-  input logic clka,
-  input logic [9:0] addra,
-  output logic [7:0] douta
-);
-    song_rom my_songs(.clka(clka), .addra(addra), .douta(douta));
-endmodule
